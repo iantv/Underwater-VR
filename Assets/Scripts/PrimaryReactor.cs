e3264621c1c50cb625e4b250e46fba8e0085fad8
vvbarilov <ian.tatiana.v@gmail.com>
2021-05-12 19:00:37 +1000

    Enable lights by pressing X button on Left Controller

---
+++ Assets/Scripts/PrimaryReactor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimaryReactor : MonoBehaviour
+{
+    public GameObject[] SpotLights = new GameObject[2];
+    public PrimaryButtonWatcher watcher;
+    public bool IsPressed = false; // used to display button state in the Unity Inspector window
+    public Vector3 rotationAngle = new Vector3(45, 45, 45);
+    public float rotationDuration = 0.25f; // seconds
+    private Quaternion offRotation;
+    private Quaternion onRotation;
+    private Coroutine rotator;
+
+
+    void Start()
+    {
+        watcher.primaryButtonPress.AddListener(onPrimaryButtonEvent);
+        Debug.Log("Primary Reactor start()");
+        offRotation = this.transform.rotation;
+        onRotation = Quaternion.Euler(rotationAngle) * offRotation;
+    }
+
+    public void onPrimaryButtonEvent(bool pressed)
+    {
+        
+        IsPressed = pressed;
+        foreach (GameObject light in SpotLights)
+        {
+            light.GetComponent<Light>().enabled = pressed;
+        }
+        if (rotator != null)
+            StopCoroutine(rotator);
+        if (pressed)
+            rotator = StartCoroutine(AnimateRotation(this.transform.rotation, onRotation));
+        else
+            rotator = StartCoroutine(AnimateRotation(this.transform.rotation, offRotation));
+    }
+
+    private IEnumerator AnimateRotation(Quaternion fromRotation, Quaternion toRotation)
+    {
+        float t = 0;
+        while (t < rotationDuration)
+        {
+            transform.rotation = Quaternion.Lerp(fromRotation, toRotation, t / rotationDuration);
+            t += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
