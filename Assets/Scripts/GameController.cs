e2d491f3a1aecab95a4ef912715fea3efd46a1ed
vvbarilov <vvbarilov78@gmail.com>
2021-05-27 14:22:33 +1000

    Add rotation and height changing

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,7 +6,10 @@ public class GameController : MonoBehaviour
 {
     public bool IsVRMode;
     public GameObject Submarine;
-    public int Speed;
+    public int DirectionSpeed;
+    private int rotationSpeed;
+    [SerializeField]
+    public int RotationSpeed;// { get => rotationSpeed; set => rotationSpeed = value; }
 
     void Start()
     {
@@ -23,25 +26,45 @@ public class GameController : MonoBehaviour
         {
             Rigidbody rb = Submarine.GetComponent<Rigidbody>();
             Vector3 force = Vector3.zero;
+            Vector3 rot = Vector3.zero;
+            Vector3 hei = Vector3.zero;
 
             if (Input.GetKey(KeyCode.W))
             {
-                force = Time.deltaTime * Speed * Submarine.transform.forward;
+                force = Time.deltaTime * DirectionSpeed * Submarine.transform.forward;
             }
             if (Input.GetKey(KeyCode.S))
             {
-                force = -1 * Time.deltaTime * Speed * Submarine.transform.forward;
+                force = -1 * Time.deltaTime * DirectionSpeed * Submarine.transform.forward;
             }
             if (Input.GetKey(KeyCode.A))
             {
-                force = -1 * Time.deltaTime * Speed * Submarine.transform.right;
+                force = -1 * Time.deltaTime * DirectionSpeed * Submarine.transform.right;
             }
             if (Input.GetKey(KeyCode.D))
             {
-                force = Time.deltaTime * Speed * Submarine.transform.right;
+                force = Time.deltaTime * DirectionSpeed * Submarine.transform.right;
             }
-
+            if (Input.GetKey(KeyCode.Q))
+            {
+                rot.z += Time.deltaTime * RotationSpeed;
+            }
+            if (Input.GetKey(KeyCode.E)) 
+            {
+                rot.z -= Time.deltaTime * RotationSpeed;
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (Submarine.transform.position.y < 46)
+                    hei.y += Time.deltaTime * 3f;
+            }
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                hei.y -= Time.deltaTime * 1f;
+            }
             rb.AddForce(force);
+            Submarine.transform.Rotate(rot);
+            Submarine.transform.Translate(hei, Space.World);
 
             if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
             {
