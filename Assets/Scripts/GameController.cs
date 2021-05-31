47de1dbb7d88242ae67e35bbccaf469482b004f6
vvbarilov <vvbarilov78@gmail.com>
2021-05-31 19:46:07 +1000

    refactor GameController script

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,27 +7,26 @@ public class GameController : MonoBehaviour
     public bool IsVRMode;
     public GameObject Submarine;
     public int DirectionSpeed;
-    private int rotationSpeed;
+    //private int rotationSpeed;
     [SerializeField]
     public int RotationSpeed;// { get => rotationSpeed; set => rotationSpeed = value; }
     public GameObject SpotLight;
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        Rigidbody rb = Submarine.GetComponent<Rigidbody>();
-        Vector3 force = Vector3.zero;
-        Vector3 rot = Vector3.zero;
-        Vector3 hei = Vector3.zero;
+        Light();
+        Translate();
+        Rotate();
+    }
 
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            SpotLight.SetActive(!SpotLight.activeSelf);
-        }
+    void Translate()
+    {
+        Vector3 force = Vector3.zero;
         if (Input.GetKey(KeyCode.W))
         {
             force = Time.deltaTime * DirectionSpeed * Submarine.transform.forward;
@@ -44,6 +43,30 @@ public class GameController : MonoBehaviour
         {
             force = Time.deltaTime * DirectionSpeed * Submarine.transform.right;
         }
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            Submarine.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            Debug.Log("Key UP!");
+        }
+
+        Vector3 hei = Vector3.zero;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            hei.y += Time.deltaTime * 3f;
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            hei.y -= Time.deltaTime * 3f;
+        }
+
+        Submarine.GetComponent<Rigidbody>().AddForce(force);
+        Submarine.transform.Translate(hei, Space.World);
+    }
+
+    void Rotate()
+    {
+        Vector3 rot = Vector3.zero;
+
         if (Input.GetKey(KeyCode.Q))
         {
             rot.z += Time.deltaTime * RotationSpeed;
@@ -52,27 +75,17 @@ public class GameController : MonoBehaviour
         {
             rot.z -= Time.deltaTime * RotationSpeed;
         }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            hei.y += Time.deltaTime * 3f;
-        }
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            hei.y -= Time.deltaTime * 1f;
-        }
+
         if (Input.GetKey(KeyCode.Mouse0))
             rot.y = Input.GetAxis("Mouse X") * 2;
         if (Input.GetKey(KeyCode.Mouse1))
-            rot.x = Input.GetAxis("Mouse Y");
-
-        rb.AddForce(force);
+            rot.x = Input.GetAxis("Mouse Y") * 2;
         Submarine.transform.Rotate(rot);
-        Submarine.transform.Translate(hei, Space.World);
+    }
 
-        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
-        {
-            rb.velocity = Vector3.zero;
-            Debug.Log("Stop submarine");
-        }
+    void Light()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+            SpotLight.SetActive(!SpotLight.activeSelf);
     }
 }
